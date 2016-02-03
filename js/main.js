9280e8241ead921fcc3533b3b0d9639d286a5ea0
catherinestreiffer <catherinestreiffer@gmail.com>
2016-02-02 21:38:52 -0600

    Module 2 activity

--- js/main.js
+++ js/main.js
@@ -1,1 +1,105 @@
-// code by Catherine E. Streiffer, 2016 //
+//initialize all functions called when the script loads
+function initialize(){
+    cities();
+    addEvents();
+    clickme();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cityPop = [
+        {
+            city: 'Memphis',
+            population: 653450
+        },
+        {
+            city: 'New Orleans',
+            population: 378715
+        },
+        {
+            city: 'St Louis',
+            population: 318416
+        },
+        {
+            city: 'Chicago',
+            population: 2719000
+        }
+    ];
+
+    //append the table element to the div
+    $("#mydiv").append("<table>");
+
+    //append a header row to the table
+    $("table").append("<tr>");
+
+    //add the "City" and "Population" columns to the header row
+    $("tr").append("<th>City</th><th>Population</th>");
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
+//}; commenting out the end of the cities function so I can add the addColumns function to it
+
+
+//beginning of debug part
+//function to add columns to the table that categorize the population by small, medium, and large
+//Instead of having a function called addColumns I added it to the cities function,
+//since it needed the local variable cityPop
+//function addColumns(cityPop){
+    //iterate over each row element
+  $('tr').each(function(i){
+
+    	if (i == 0){
+              //add the city size column to the header row
+              $(this).append('<th>City Size</th>');
+    	   } else {
+              //if it's not the header row, calculate the citySize based on the cityPop.population
+              //this i is one off from the cityPop i because the header was added
+              //differently, so do cityPop[i-1]
+              var citySize;
+                  if (cityPop[i-1].population < 100000){
+    			             citySize = 'Small';
+                  } else if (cityPop[i-1].population < 500000){
+    			             citySize = 'Medium';
+    		          } else {
+    			             citySize = 'Large';
+              	  };
+      //put the calculated citySize in the table
+    	$(this).append('<td>' + citySize + '</td>');
+    };
+});
+};
+//function to change the text color each time you mouseover the table
+function addEvents(){
+	$('table').mouseover(function(){
+      //this is a pretty funny way to choose a color. You want to end up with a rgb color,
+      //which has three parts to it, each integers up to 255. this randomly chooses them,
+      //putting the commas and end parentheses in as it goes through the loop.
+      var color = "rgb(";
+      for (var i=0; i<3; i++){
+          var random = Math.round(Math.random() * 255);
+          color += random;
+          if (i<2){
+				        color += ",";
+			    } else {
+				        color += ")";
+		  };
+
+  //change the text color to whatever the var color is set to be
+	$(this).css('color', color);
+    };
+  });
+};
+
+//when you click on the table a popup says Hey, you clicked me
+function clickme(){
+		alert('Hey, you clicked me!');
+    $('table').on('click', clickme);
+};
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
